8adac2b55f169bebfa8cbbb4f85a4b9f614a117b
TheYorsher <stepanov2003@mail.ru>
2019-12-04 15:58:29 +0300

    Initial commit

---
+++ src/com/company/Bicycle.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Bicycle extends Transport {
+    @Override
+    public void Beep() {
+        System.out.println("Drin-Drin");
+    }
+}
---
+++ src/com/company/Car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Car extends Transport {
+
+
+    @Override
+    public void Beep() {
+        System.out.println("Bee-Bee");
+    }
+}
+
